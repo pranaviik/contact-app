ea679f681d6303c39f0d2e67c749eb63f68e1036
Pranavi Kantreddi <pranavikantreddi@Pranavis-MacBook-Pro.local>
2026-02-05 17:13:54 +0530

    change

---
+++ docs/sw.js
@@ -0,0 +1,57 @@
+// sw.js - Service Worker
+
+// You can change this version number to force a cache update
+const CACHE_NAME = 'contact-app-v1';
+
+// List of files to cache
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js',
+    './manifest.json',
+    // Include your icons if you want them cached for offline use
+    // './icon-192.png',
+    // './icon-512.png'
+];
+
+// Install event: Cache files
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Fetch event: Serve from cache, fall back to network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
