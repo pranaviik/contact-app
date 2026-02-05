df4c03b5f7e4d4f045cb966809dac8114185516a
Pranavi Kantreddi <pranavikantreddi@Pranavis-MacBook-Pro.local>
2026-02-05 17:56:34 +0530

    new

--- docs/app.js
+++ docs/app.js
@@ -644,7 +644,7 @@ $(function () {
         if (!number.trim()) return;
         logCall(number);
         $keypadDisplay.text('');
-        alert('Simulated call to ' + number);
+        window.location.href='tel'+number;
     });
 
     // --- Initial load ---
